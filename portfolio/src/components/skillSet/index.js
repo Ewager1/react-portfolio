df6c86bd92d5b078c5549c83b6b9b476dbd8606a
EWager <ethanbwager@gmail.com>
2021-03-01 09:05:34 -0500

    created all skill-set icons in skillSet file

--- portfolio/src/components/skillSet/index.js
+++ portfolio/src/components/skillSet/index.js
@@ -4,14 +4,68 @@ import { Row } from "react-bootstrap";
 import SkillSetIcon from "../skillSetIcon";
 
 function SkillSet() {
-  return <>
-   <Row>
-      <SkillSetIcon
-        iconText="React"
-        altText="React Icon"
-        imgUrl="/icons/reactIcon.png"
-      />
-    </Row>
-  </>
+  return (
+    <>
+      <Row>
+        <SkillSetIcon
+          iconText="React"
+          altText="React Icon"
+          imgUrl="/icons/reactIcon.png"
+        />
+
+        <SkillSetIcon
+          iconText="Javascript"
+          altText="Javascript Icon"
+          imgUrl="/icons/jsIcon.png"
+        />
+
+        <SkillSetIcon
+          iconText="Bootstrap"
+          altText="Bootstrap Icon"
+          imgUrl="/icons/bootstrapIcon.png"
+        />
+
+        <SkillSetIcon
+          iconText="CSS3"
+          altText="CSS Icon"
+          imgUrl="/icons/cssIcon.png"
+        />
+        <SkillSetIcon
+          iconText="Sql"
+          altText="Sql Icon"
+          imgUrl="/icons/mySQLIcon.png"
+        />
+      </Row>
+      <Row>
+        <SkillSetIcon
+          iconText="NodeJs"
+          altText="NodeJs Icon"
+          imgUrl="/icons/nodeJsIcon.png"
+        />
+        <SkillSetIcon
+          iconText="MongoDb"
+          altText="MongoDb Icon"
+          imgUrl="/icons/mongoDb Icon.png"
+        />
+        <SkillSetIcon
+          iconText="Progressive Web App"
+          altText="Aos Icon"
+          imgUrl="/icons/pwaIcon.png"
+        />
+
+        <SkillSetIcon
+          iconText="Github"
+          altText="Github Icon"
+          imgUrl="/icons/github-icon.png"
+        />
+
+        <SkillSetIcon
+          iconText="Rest Api"
+          altText="Rest Api Icon"
+          imgUrl="/icons/restApiIcon.png"
+        />
+      </Row>
+    </>
+  );
 }
 export default SkillSet;
